9bfca54414eb828334f5d74d82a35189cee49681
VuKhai <vqkhai93@gmail.com>
2018-07-27 15:26:39 +0700

    create login page v3

--- toeic-core-dao-impl/src/main/java/vn/myclass/core/daoimpl/UserDaoImpl.java
+++ toeic-core-dao-impl/src/main/java/vn/myclass/core/daoimpl/UserDaoImpl.java
@@ -1,9 +1,51 @@
-﻿package vn.myclass.core.daoimpl;
+package vn.myclass.core.daoimpl;
 
+import org.hibernate.*;
+import vn.myclass.core.common.utils.HibernateUtil;
 import vn.myclass.core.dao.UserDao;
 import vn.myclass.core.data.daoimpl.AbstractDao;
 import vn.myclass.core.persistence.entity.UserEntity;
 
+
 public class UserDaoImpl extends AbstractDao<Integer, UserEntity> implements UserDao {
 
+    public UserEntity isUserExist(String name, String password) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        UserEntity entity = new UserEntity();
+        try{
+            StringBuilder sql = new StringBuilder("FROM UserEntity WHERE name= :name AND password = :password");
+            Query query = session.createQuery(sql.toString());
+            query.setParameter("name", name);
+            query.setParameter("password", password);
+            entity = (UserEntity) query.uniqueResult();
+            transaction.commit();
+        }catch (HibernateException e){
+            transaction.rollback();
+            throw e;
+        }finally {
+            session.close();
+        }
+        return entity;
+    }
+
+    public UserEntity findRoleByUser(String name, String password) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        UserEntity entity = new UserEntity();
+        try{
+            StringBuilder sql = new StringBuilder("FROM UserEntity WHERE name= :name AND password = :password");
+            Query query = session.createQuery(sql.toString());
+            query.setParameter("name", name);
+            query.setParameter("password", password);
+            entity = (UserEntity) query.uniqueResult();
+            transaction.commit();
+        }catch (HibernateException e){
+            transaction.rollback();
+            throw e;
+        }finally {
+            session.close();
+        }
+        return entity;
+    }
 }
--- toeic-core-dao/src/main/java/vn/myclass/core/dao/UserDao.java
+++ toeic-core-dao/src/main/java/vn/myclass/core/dao/UserDao.java
@@ -1,11 +1,9 @@
-﻿package vn.myclass.core.dao;
+package vn.myclass.core.dao;
 
 import vn.myclass.core.data.dao.GenericDao;
-import vn.myclass.core.persistence.entity.RoleEntity;
 import vn.myclass.core.persistence.entity.UserEntity;
 
 public interface UserDao extends GenericDao<Integer, UserEntity> {
-
-
+    UserEntity isUserExist(String name, String password);
+    UserEntity findRoleByUser(String name, String password);
 }
-
